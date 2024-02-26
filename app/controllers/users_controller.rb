024587362a57a9b08b9b9aaa32ec854f6219d147
Trieu Tien Nguyen <nguyenthanhtung111xxx@gmail.com>
2024-02-26 22:26:23 +0700

    Update chapter/7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,29 +5,25 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     return @user if @user
 
-    flash[:errors] = t("not_found.user")
+    flash[:error] = t("not_found.user")
     redirect_to root_path
   end
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
-    @user = User.create(user_params)
-    handle_user_save_result(@user)
+    @user = User.create user_params
+    if @user.save
+      flash[:success] = t("create.user.success_message", name: t("create.user.label_name"))
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
-
-  def handle_user_save_result(user)
-    if user.save
-      flash[:success] = t("create.user.success_message", name: t("create.user.label_name"))
-      redirect_to user
-    else
-      flash[:errors] = user.errors.full_messages
-      flash[:errors_count] = user.errors.count
-      redirect_to sign_up_path
-    end
-  end
 
   def user_params
     params.permit(:name, :birthday, :password, :password_confirmation)
